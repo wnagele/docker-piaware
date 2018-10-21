94a54b4309fd50382aa8a244258e936bb52ef3ce
Jean-Ralph Aviles <jeanralph.aviles@gmail.com>
2018-10-21 05:49:15 -0700

    Allow setting of Flightaware credentials by environment variable.

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-if [ -z "$2" ]; then
-  echo "Missing arguments: <flightaware user> <flightaware password>"
+if [ "$#" -eq 2 ]; then
+  FLIGHTAWARE_USER="$1"
+  FLIGHTAWARE_PASS="$2"
+elif [ "$#" -gt 2 ]; then
+  echo "Too many arguments. Usage: [<flightaware user> <flightaware password>]"
+  exit 1
+elif [ "$#" -gt 0 ]; then
+  echo "Too few arguments. Usage: [<flightaware user> <flightaware password>]"
   exit 1
 fi
 if [ -z "${BEAST_PORT_30005_TCP_ADDR}" ]; then
@@ -8,9 +14,9 @@ if [ -z "${BEAST_PORT_30005_TCP_ADDR}" ]; then
   exit 1
 fi
 
-/usr/bin/piaware-config flightaware-user "$1"
-/usr/bin/piaware-config flightaware-password "$2"
-/usr/bin/piaware-config allow-mlat "${MLAT}"
+/usr/bin/piaware-config flightaware-user "${FLIGHTAWARE_USER:?}"
+/usr/bin/piaware-config flightaware-password "${FLIGHTAWARE_PASS:?}"
+/usr/bin/piaware-config allow-mlat "${MLAT:=no}"
 
 socat TCP-LISTEN:30005,fork TCP:${BEAST_PORT_30005_TCP_ADDR}:${BEAST_PORT_30005_TCP_PORT:-30005} &
 /usr/bin/piaware -debug
